077204a46203d9fd6a53b20ed015f9dcef4c6f04
Louise <lhoepfner@gmail.com>
2019-01-17 13:40:04 +0100

    Added zombie example

--- p5PlayFun/sketch.js
+++ p5PlayFun/sketch.js
@@ -1,56 +1,27 @@
-var zombie;
+var block;
 
-function setup() {
-  createCanvas(800, 300);
+function setup(){
+  createCanvas(800, 400);
+  //rectMode(CENTER);
 
-  //create a sprite and add the 2 animations
-  zombie = createSprite(400, 150, 50, 100);
-
-  //label, first frame, last frame
-  //the addAnimation method returns the added animation
-  //that can be store in a temporary variable to change parameters
-  var myAnimation = zombie.addAnimation('floating', 'assets/zombiegirl/Idle0001.png', 'assets/zombiegirl/Idle0015.png');
-  //offX and offY is the distance of animation from the center of the sprite
-  //in this case since the animations have different heights i want to adjust
-  //the vertical offset to make the transition between floating and moving look better
-  myAnimation.offY = 18;
-
-  zombie.addAnimation('moving', 'assets/zombiegirl/Walk0001.png', 'assets/zombiegirl/Walk0010.png');
-
+  block = createSprite(30, 30);
+  block.draw = function(){
+    fill(100, 120, 40);
+    rect(0, 0, 30, 30);
+  }
 
 }
 
-function draw() {
+function draw(){
   background(255, 255, 255);
 
-  //if mouse is to the left
-  if(mouseX < zombie.position.x - 10) {
-    zombie.changeAnimation('moving');
-    //flip horizontally
-    zombie.mirrorX(-1);
-    //negative x velocity: move left
-    zombie.velocity.x = -2;
-  }
-  else if(mouseX > zombie.position.x + 10) {
-    zombie.changeAnimation('moving');
-    //unflip
-    zombie.mirrorX(1);
-    zombie.velocity.x = 2;
-  }
-  else {
-    //if close to the mouse, don't move
-    zombie.changeAnimation('floating');
-    zombie.velocity.x = 0;
-  }
-
-  //up and down keys to change the scale
-  //note that scaling the image quality deteriorates
-  //and scaling to a negative value flips the image
-  if(keyIsDown(UP_ARROW))
-    zombie.scale += 0.05;
-  if(keyIsDown(DOWN_ARROW))
-    zombie.scale -= 0.05;
-
-  //draw the sprite
   drawSprites();
+  moveBlock();
+  // block.debug = true;
+}
+
+function moveBlock(){
+  block.velocity.x = (mouseX-block.position.x)/10;
+  block.velocity.y = (mouseY-block.position.y)/10;
+
 }
